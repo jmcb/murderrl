84501fa93e3e2a73bedae9b008d3a78c51b7e13a
Jon <jonathan@acss.net.au>
2011-01-29 11:47:36 +1000

    Include ColourLibrary in colour.
    It's a much cleaner way of including the base colours in the actual
    library.

--- library/colour.py
+++ library/colour.py
@@ -25,31 +25,146 @@ class BaseColour (object):
 
         return True
 
+    def lighten (self):
+        """
+        Performs an in-place lightening of the current colour. This function can
+        only be currently applied to colours that return True for ``is_base()``.
+        """
+        if "dark" in self._colour:
+            self._colour = self._colour.replace("dark", "light")
+        else:
+            if self._colour == "brown":
+                self._colour = "yellow"
+            elif self._colour == "light gray":
+                self._colour = "white"
+
+    def lightened (self):
+        """
+        Return a copy of the current colour that has been lightened.
+        """
+        c = self.copy()
+        c.lighten()
+        return c
+
+    def darken (self):
+        """
+        Perform an in-place darkening of the current colour. This function can
+        only be applied to colours that return True for ``is_base()``.
+        """
+        if "light" in self._colour:
+            self._colour = self._colour.replace("light", "dark")
+        else:
+            if self._colour == "yellow":
+                self._colour = "brown"
+            elif self._colour == "white":
+                self._colour = "light gray"
+
+    def darkened (self):
+        """
+        Return a copy of the current colour which has been darkened.
+        """
+        c = self.copy()
+        c.darken()
+        return c
+
+    def copy (self):
+        """
+        Return a copy of the current colour.
+        """
+        c = BaseColour(self._colour)
+        return c
+
     def __rerp__ (self):
         return "<Colour: %s>" % self._colour
 
     def __str__ (self):
         return self._colour
 
-BLACK = BaseColour("black")
-BLUE = BaseColour("dark blue")
-GREEN = BaseColour("dark green")
-CYAN = BaseColour("dark cyan")
-RED = BaseColour("dark red")
-MAGENTA = BaseColour("dark magenta")
-BROWN = BaseColour("brown")
-LIGHTGRAY = BaseColour("light gray")
-LIGHTGREY = LIGHTGRAY
-DARKGRAY = BaseColour("dark gray")
-DARKGREY = DARKGRAY
-LIGHTBLUE = BaseColour("light blue")
-LIGHTGREEN = BaseColour("light green")
-LIGHTCYAN = BaseColour("light cyan")
-LIGHTRED = BaseColour("light red")
-LIGHTMAGENTA = BaseColour("light magenta")
-YELLOW = BaseColour("yellow")
-WHITE = BaseColour("white")
-DEFAULT = BaseColour("default")
+    def __cmp__ (self, other):
+        """
+        Allow for rich comparison between colours.
+
+        :``other``: The colour being compared to. Can either be an instance of
+                    BaseColour, in which case colour names are compared, or
+                    Colour, in which case it attempts to compare colour names
+                    with the Colour's current foreground colour.
+        """
+        if isinstance(other, BaseColour):
+            return cmp(self._colour, other._colour)
+        elif isinstance(other, Colour):
+            if hasattr(other._foreground, "_colour"):
+                return cmp(self._colour, other._foreground._colour)
+
+        return cmp(self, other)
+
+class ColourLibrary (object):
+    """
+    A collection of standardised Base colours contained within a specific library.
+    """
+    _colours = None
+    def __init__ (self):
+        """
+        Initialise the blank library.
+        """
+        self._colours = []
+
+    def add (self, name, colour=None):
+        """
+        Include a specific colour in the library.
+
+        :``name``: The name of the colour.
+        :``colour``: An instance of BaseColour. If ``None``, a new BaseColour
+                     will be initialised using ``name`` as the colour name.
+        """
+        if colour is None:
+            colour = BaseColour(name)
+
+        if not isinstance(colour, BaseColour):
+            colour = BaseColour(colour)
+
+        self._colours.append(colour)
+
+        setattr(self, name.lower(), colour)
+        setattr(self, name.upper(), colour)
+
+    def find (self, name):
+        """
+        Find a specific colour within the library.
+
+        :``name``: The name of the colour being searched for.
+        """
+        if hasattr(self, name.lower()):
+            return getattr(self, name.lower())
+
+        for colour in self._colours:
+            if name in str(colour):
+                return colour
+
+        return None
+
+Colours = ColourLibrary()
+
+Colours.add("black")
+Colours.add("blue", "dark blue")
+Colours.add("green", "dark green")
+Colours.add("cyan", "dark cyan")
+Colours.add("red", "dark red")
+Colours.add("magenta", "dark magenta")
+Colours.add("brown")
+Colours.add("lightgray", "light gray")
+Colours.add("lightgrey", "light gray")
+Colours.add("darkgray", "dark gray")
+Colours.add("darkgrey", "dark gray")
+Colours.add("lightblue", "light blue")
+Colours.add("lightblue", "light green")
+Colours.add("lightcyan", "light cyan")
+Colours.add("lightred", "light red")
+Colours.add("lightmagenta", "light magenta")
+Colours.add("yellow")
+Colours.add("white")
+Colours.add("default")
+
+Colors = Colours
 
 class Colour (object):
     """
@@ -67,8 +182,9 @@ class Colour (object):
         """
         assert not (foreground is None and background is None and style is None)
 
-        assert background.is_base() and background in (DEFAULT, BLACK, RED,
-            GREEN, BROWN, BLUE, MAGENTA, CYAN, LIGHTGRAY)
+        assert background.is_base() and background in (Colours.DEFAULT,
+           Colours.BLACK, Colours.RED, Colours.GREEN, Colours.BROWN, Colours.BLUE,
+            Colours.MAGENTA, Colours.CYAN, Colours.LIGHTGRAY)
 
         assert style in ("bold", "underline", "blink", "standout")
 
