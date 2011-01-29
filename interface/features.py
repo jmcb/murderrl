7cc80f1ab49495d3dc2137522b00a4b47641594d
Jon <jonathan@acss.net.au>
2011-01-29 12:24:20 +1000

    Extend the features list to include some other features.
    This isn't all of the ones that we discussed, just the ones that I could
    remember. The syntax is "glyph", colour, name, description, and a
    boolean value determining if the glyph is passable.

--- interface/features.py
+++ interface/features.py
@@ -5,11 +5,28 @@ features, a short library of specifically defined features.
 from library.feature import TextFeature
 from library.colour import Colours
 
-WALL = TextFeature("#", Colours.WHITE)
-FLOOR = TextFeature(".", Colours.LIGHTGRAY)
-DOOR = TextFeature("+", Colours.WHITE)
-STAIR_UP = TextFeature("<", Colours.WHITE)
-STAIR_DOWN = TextFeature(">", Colours.WHITE)
-WINDOW_V = TextFeature("|", Colours.WHITE)
-WINDOW_H = TextFeature("-", Colours.WHITE)
-TREE = TextFeature("&", Colours.GREEN)
+# Floor-related glyphs.
+FLOOR = TextFeature(".", Colours.LIGHTGRAY, "floor", "Floor.", True)
+GRASS = TextFeature(".", Colours.GREEN, "grass", "Grass.", True)
+COBBLES = TextFeature(",", Colours.BROWN, "cobbles", "Cobblestones.", True)
+
+# Wall-related glyphs.
+WALL = TextFeature("#", Colours.WHITE, "wall", "A wall.", False)
+CLOSED_DOOR = TextFeature("+", Colours.WHITE, "closed door", "A closed door.", False)
+OPEN_DOOR = TextFeature("'", Colours.WHITE, "open door", "An open door.", True)
+WINDOW_V = TextFeature("|", Colours.WHITE, "window", "A window.", False)
+WINDOW_H = TextFeature("-", Colours.WHITE, "window", "A window.", False)
+
+# Stairs.
+STAIR_UP = TextFeature("<", Colours.WHITE, "stairs up", "A staircase leading up.", True)
+STAIR_DOWN = TextFeature(">", Colours.WHITE, "stairs down", "A staircase leading down.", True)
+
+# Furniture
+FIREPLACE = TextFeature("]", Colours.LIGHTRED, "fireplace", "A fireplace.", False)
+CUPBOARD = TextFeature("[", Colours.BROWN, "cupboard", "A cupboard.", False)
+CHAIR = TextFeature('\\', Colours.BROWN, "chair", "A chair.", True)
+TABLE = TextFeature('=', Colours.BROWN, "table", "A table.", False)
+DESK = TextFeature('=', Colours.BROWN, "desk", "A desk.", False)
+
+# Plants, etc.
+TREE = TextFeature("&", Colours.GREEN, "tree", "A tree.", True)
