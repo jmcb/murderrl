87c7eded008600f3c8b5fda20b2808947eab7c8d
Jon <jonathan@acss.net.au>
2011-01-30 21:54:22 +1000

    Also note if they are or are not available.

--- interface/console/curse/__init__.py
+++ interface/console/curse/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
+UNAVAILABLE = False
+
 try:
     from _curse import *
 except:
     put, get, clear, init, deinit = [lambda *a: None] * 5
+    UNAVAILABLE = True
--- interface/console/win32/__init__.py
+++ interface/console/win32/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
+UNAVAILABLE = False
+
 try:
     from _win32 import *
 except:
     put, get, clear, init, deinit = [lambda *a: None] * 5
+    UNAVAILABLE = True
