8a11e63f94d2183a194f9f4490b02f02783d2863
Jonathan McManus <jonathan@acss.net.au>
2011-01-31 22:38:34 +1000

    The basis for the win32 console module.
    Nowhere near complete, but it's starting to look doable.

--- interface/console/win32/_win32.py
+++ interface/console/win32/_win32.py
@@ -1,8 +1,162 @@
 #!/usr/bin/env python
 import library.coord as coord
+import library.colour
+import ctypes
+from _subprocess import INFINITE, WAIT_OBJECT_0
+
+OLD_SCREEN_SIZE = None
+
+COMMON_LVB_UNDERSCORE = 32768
+STD_INPUT_HANDLE = -10
+STD_OUTPUT_HANDLE = -11
+STD_ERROR_HANDLE = -12
+
+WAIT_TIMEOUT = 0x102
+WAIT_ABANDONED = 0x80
+
+class DOSCoord(coord.Coord):
+    def as_dos (self):
+        return _COORD(self.x, self.y)
+
+class _COORD(ctypes.Structure):
+    _fields_ = [
+        ("X", ctypes.c_short),
+        ("Y", ctypes.c_short)]
+
+class _SMALL_RECT(ctypes.Structure):
+    _fields_ = [
+        ("Left", ctypes.c_short),
+        ("Top", ctypes.c_short),
+        ("Right", ctypes.c_short),
+        ("Bottom", ctypes.c_short)]
+
+class _CONSOLE_SCREEN_BUFFER_INFO(ctypes.Structure):
+    _fields_ = [
+        ("dwSize", _COORD),
+        ("dwCursorPosition", _COORD),
+        ("wAttributes", ctypes.c_ushort),
+        ("srWindow", _SMALL_RECT),
+        ("dwMaximumWindowSize", _COORD)]
+
+class _CONSOLE_CURSOR_INFO (ctypes.Structure):
+    _fields_ = [
+        ("dwSize", ctypes.c_ushort),
+        ("bVisible", ctypes.c_bool)]
+
+class DOSBaseColour (library.colour.BaseColour):
+    def __init__ (self, colour):
+        super(DOSBaseColour, self).__init__(colour._colour, colour._colour_id)
+
+    def is_background ():
+        if self._colour_id == 0x0000:
+            return True
+
+        return self._colour_id & 0x0070
+
+    def is_foreground ():
+        if self._colour_id == 0x0000:
+            return True
+
+        return self._colour_id & 0x0007
+
+    def as_background ():
+        if self.is_background():
+            return self._colour_id & 0x0070
+
+        return self._colour_id & 0x0007 << 4
+
+    def as_foreground ():
+        if self.is_foreground():
+            return self._colour_id & 0x0007
+
+        return self._colour_id & 0x0070 >> 4
+
+class DOSColour (library.colour.Colour):
+    def __init__ (self, colour):
+        super(DOSColour, self).__init__(colour._foreground, colour._background, colour._style)
+
+    def as_dos ():
+        fg = DOSBaseColour(self._foreground).as_foreground()
+        bg = DOSBaseColour(self._background).as_background()
+
+        new_col = fg | bg
+
+        if self._style == "underline":
+            new_col |= COMMON_LVB_UNDERSCORE
+
+        return new_col
+
+def _STDOUT ():
+    return _GetStdHandle(STD_OUTPUT_HANDLE)
+
+def _STDIN ():
+    return _GetStdHandle(STD_INPUT_HANDLE)
+
+def _STDERR ():
+    return _GetStdHandle(STD_ERROR_HANDLE)
+
+def _GetStdHandle (handle):
+    return ctypes.windll.kernel32.GetStdHandle(handle)
+
+def _GetColour ():
+    return _GetConsoleScreenBufferInfo().wAttributes
+
+def _GetBG ():
+    return _GetConsoleScreenBufferInfo().wAttributes & 0x0070
+
+def _GetFG ():
+    return _GetConsoleScreenBufferInfo().wAttributes & 0x0007
+
+def _WaitForMultipleObjects (handles, wait_all=False, wtime=INFINITE):
+    return ctypes.windll.kernel32.WaitForMultipleObjects(len(handles), (ctypes.c_int*len(handles))(*handles), ctypes.c_bool(wait_all), wtime)
+
+def _GetConsoleScreenBufferInfo ():
+    buffer_info = _CONSOLE_SCREEN_BUFFER_INFO()
+    ctypes.windll.kernel32.GetConsoleScreenBufferInfo(_STDOUT(), ctypes.byref(buffer_info))
+    return buffer_info
+
+def _GetConsoleCursorInfo ():
+    cursor_info = _CONSOLE_CURSOR_INFO()
+    ctypes.windll.kernel32.GetConsoleCursorInfo(_STDOUT(), ctypes.byref(cursor_info))
+    return cursor_info
+
+def _SetColour (colour, dos=False):
+    if not dos:
+        colour = DOSColour(colour).as_dos()
+    return ctypes.windll.kernel32.SetConsoleTextAttribute(_STDOUT(), colour)
+
+def _SetConsoleCursorInfo (size=1, visible=True):
+    cursor_info = _CONSOLE_CURSOR_INFO()
+    cursor_info.dwSize = size
+    cursor_info.bVisible = ctypes.c_bool(visible)
+    return ctypes.windll.kernel32.SetConsoleCursorInfo(_STDOUT(), cursor_info)
+
+def _SetConsoleSize (size=coord.Size(80, 25)):
+    c = DOSCoord(size).as_dos()
+    return ctypes.windll.kernel32.SetConsoleScreenBufferSize(_STDOUT(), c)
+
+def _goto (c):
+    c = DOSCoord(c).as_dos()
+    return ctypes.windll.kernel32.SetConsoleCursorPosition(_STDOUT(), c)
+
+def _getxy ():
+    c = DOSCoord()
+    act_coord = _GetConsoleScreenBufferInfo().dwCursorPosition
+    c.x = act_coord.X
+    c.y = act_coord.Y
+    return c
 
 def put (char, c, colour=None):
-    pass
+    _goto(c)
+    old_c = _getxy()
+    old_colour = _GetColour()
+    if colour is not None:
+        _SetColour(colour)
+
+    print char
+
+    _goto(old_c)
+    _SetColour(old_colour, True)
 
 def get (err=False, block=False):
     pass
@@ -15,10 +169,17 @@ def clear (char=None, colour=None):
             put (char, coord.Coord(x, y), colour)
 
 def init ():
-    pass
+    global OLD_SCREEN_SIZE
+    OLD_SCREEN_SIZE = _GetConsoleScreenBufferInfo().dwSize
+    OLD_SCREEN_SIZE = coord.Coord(OLD_SCREEN_SIZE.X, OLD_SCREEN_SIZE.Y)
+    _SetConsoleSize()
 
 def deinit ():
-    pass
+    _SetConsoleSize(OLD_SCREEN_SIZE)
 
 def size ():
-    pass
+    info = _GetConsoleScreenBufferInfo().dwSize
+    size = coord.Size()
+    size.width = info.x
+    size.height = info.y
+    return size
