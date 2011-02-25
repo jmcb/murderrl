1b6e62c25c7452729231fb0c6b2e8b7e2e1aa6a0
Johanna Ploog <j-p-e-g@users.sourceforge.net>
2011-02-26 00:48:29 +0100

    Get rid of a redundant method definition.

--- test_walk.py
+++ test_walk.py
@@ -7,16 +7,13 @@ Toggle canvas vs. feature view with 't'. (Should be identical.)
 import curses
 
 import builder.manor
-import library.viewport, library.coord
+from library import viewport, coord
 import interface.console
 from interface.features import *
+from interface.output import *
 from library.feature import *
 
 screen = interface.console.select()
-
-def put_text (text, spot):
-    for ind, char in enumerate(text):
-        screen.put(char, library.coord.Coord(spot.x+ind, spot.y))
 
 def is_traversable (feature_grid, pos):
     return feature_grid.__getitem__(pos).traversable()
@@ -39,22 +36,22 @@ def main ():
     manor = base_manor.combine()
 
     # Draw features on canvas.
-    for pos in library.coord.RectangleIterator(manor.size()):
+    for pos in coord.RectangleIterator(manor.size()):
         feat = base_manor.get_feature(pos)
         if feat != NOTHING and feat != WALL and feat != FLOOR:
             manor.__setitem__(pos, feat.glyph())
 
     # Initialise the view port.
-    vp = library.viewport.ViewPort(buffer=manor,
-                                   width =min(manor.size().width, 70),
-                                   height=min(manor.size().height, 20))
+    vp = viewport.ViewPort(buffer=manor,
+                           width =min(manor.size().width, 70),
+                           height=min(manor.size().height, 20))
 
     # Initially place the player in the centre of the entrance hall.
     ehall = base_manor.get_room(base_manor.entrance_hall)
-    real_pos = library.coord.Coord(ehall.pos().x + ehall.size().x/2, ehall.pos().y + ehall.size().y/2)
+    real_pos = coord.Coord(ehall.pos().x + ehall.size().x/2, ehall.pos().y + ehall.size().y/2)
 
     # Initialise a couple of other variables.
-    last_move = library.coord.Coord(0, 0) # the last step taken by the player
+    last_move = coord.Coord(0, 0) # the last step taken by the player
     move_was_blocked = False   # bumped into a wall
     did_move         = True    # actually took a step
     print_features   = False   # draw manor via the feature grid
@@ -69,31 +66,31 @@ def main ():
         # Depending on the current toggle state (toggle key 't'), either draw
         # the manor via the feature grid, or via the shape canvas.
         if print_features:
-            for coord in library.coord.RectangleIterator(sect.size()):
-                if coord >= base_manor.features.size():
+            for pos in coord.RectangleIterator(sect.size()):
+                if pos >= base_manor.features.size():
                     continue
-                real_coord = coord + library.coord.Coord(vp._left, vp._top)
+                real_coord = pos + coord.Coord(vp._left, vp._top)
                 char = base_manor.features.__getitem__(real_coord).glyph()
-                screen.put(char, coord+1)
+                screen.put(char,pos+1)
         else:
-            for coord, char in sect:
+            for pos, char in sect:
                 if char == None:
                     char = " "
-                screen.put(char, coord+1)
+                screen.put(char, pos+1)
 
-        canvas_pos = library.coord.Coord(real_pos.x - vp._left, real_pos.y - vp._top)
+        canvas_pos = coord.Coord(real_pos.x - vp._left, real_pos.y - vp._top)
         # Draw the player.
         screen.put("@", canvas_pos + 1)
 
         if move_was_blocked:
-            put_text("Ouch! You bump into a %s!" % tried_move_feat.name(), library.coord.Coord(0, 22))
+            print_line("Ouch! You bump into a %s!" % tried_move_feat.name(), coord.Coord(0, 22))
         elif not did_move:
             mode = "canvas view"
             if print_features:
                 mode = "feature grid"
-            put_text("Switched to %s." % mode, library.coord.Coord(0, 22))
+            print_line("Switched to %s." % mode, coord.Coord(0, 22))
         elif base_manor.features.__getitem__(real_pos) == CLOSED_DOOR:
-            put_text("You see here a door.", library.coord.Coord(0, 22))
+            print_line("You see here a door.", coord.Coord(0, 22))
 
         ## Debugging information.
 
@@ -104,9 +101,9 @@ def main ():
             id   = base_manor.get_room_index(real_pos +  1)
             type = "room"
         room_desc = base_manor.get_roomprop(id)
-        put_text("Sect size : %s, Start coord: %s, Stop coord: %s, %s" % (sect.size(), library.coord.Coord(vp._left, vp._top), library.coord.Coord(vp._left + vp._width, vp._top + vp._height), room_desc), library.coord.Coord(0, 23))
+        print_line("Sect size : %s, Start coord: %s, Stop coord: %s, %s" % (sect.size(), coord.Coord(vp._left, vp._top), coord.Coord(vp._left + vp._width, vp._top + vp._height), room_desc), coord.Coord(0, 23))
 
-        put_text("Manor size: %s, Player coord: %s, last_move: %s, %s id: %s" % (manor.size(), real_pos + 1, last_move, type, id), library.coord.Coord(0, 24))
+        print_line("Manor size: %s, Player coord: %s, last_move: %s, %s id: %s" % (manor.size(), real_pos + 1, last_move, type, id), coord.Coord(0, 24))
 
         # Get a key.
         ch = screen.get(block=True)
@@ -116,7 +113,7 @@ def main ():
         # Otherwise, take a step unless it would make us leave the manor.
 
         # Reinitialise the relevant variables.
-        last_move        = library.coord.Coord(0, 0)
+        last_move        = coord.Coord(0, 0)
         move_was_blocked = False
         did_move         = True
         tried_move_feat  = NOTHING
@@ -177,8 +174,8 @@ def main ():
 
         if move_was_blocked:
             # Reset last_move.
-            last_move = library.coord.Coord(0, 0)
-            did_move = False
+            last_move = coord.Coord(0, 0)
+            did_move  = False
 
     screen.deinit()
 
