1333aa9e8c935318d5106f7346ec62b73749529a
Jon <jonathan@acss.net.au>
2011-03-01 22:43:04 +1000

    Start work on the "screen" object and interface.
    It's nowhere near complete, but at the very least there is now some
    sort of abstraction between the actual console drawing interface,
    and how we print to it.
    
    Eventually it needs to be able to handle multiple views, but initially
    the main thing that I'm working on is a scrolling "message" region,
    a "fixed" region for displaying the main information about the game,
    and a viewport region for actually displaying the manor.

---
+++ interface/screen.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+"""
+The "screen" is the rawest part of the interface. It consists entirely of a
+grid of characters, equivalent to the width and height of the current display.
+"""
+
+from library import shape, coord
+
+class Grid (object):
+    _grid = None
+    def __init__ (self, width=0, height=0):
+        self._grid = []
+
+        for row in xrange(height):
+            row = []
+            for column in xrange(width):
+                row.append(None)
+            self._grid.append(row)
+
+    def at (self, c, x=None):
+        if x is not None:
+            c = coord.Coord(coord, x)
+
+        try:
+            return self._grid[c.y][c.x]
+        except IndexError:
+            return None
+
+    def set (self, c, x=None, grid=None):
+        if x is not None:
+            c = coord.Coord(c, x)
+        else:
+            grid = x
+
+        try:
+            self._grid[c.y][c.x] = grid
+        except IndexError:
+            return False
+
+        return True
+
+class ScreenGrid (shape.Shape):
+    pass
+
+class ColourGrid (Grid):
+    pass
+
+class Region (object):
+    start = None
+    stop = None
+    name = None
+    def __init__ (self, start, stop, name, screen):
+        self.start = start
+        self.stop = stop
+        self.name = name
+        self.screen = screen
+    def size (self):
+        return coord.Size(self.stop - self.start)
+
+    def blit (self, shape):
+        """
+        This function prints of much as `shape` as it can within the bounds
+        described by this range. This information is "written" to the screen,
+        and is therefore immediately visible. It does not wipe out any colours
+        in the area controlled by this image.
+        """
+        if shape.size() < shape.size():
+            for index, char in shape:
+                self.screen.glyphs()[index+self.start] = char
+
+class Screen (object):
+    _regions = None
+
+    def __init__ (self, size, phys_screen):
+        width, height = size
+
+        self.glyphs = ScreenGrid(width=width, height=height, fill=" ")
+        self.colours = ColourGrid(width=width, height=height)
+
+        self._regions = []
+
+        self.screen = phys_screen
+
+    def region (self, start, stop=None, name=None):
+        if stop is not None:
+            new_region = Region(start, stop, name)
+        else:
+            new_region = start
+            name = stop
+
+        self._regions.append(new_region)
+
+    def regions (self, index=None):
+        if index is not None:
+            try:
+                return self._regions[index]
+            except IndexError:
+                return self.region_by_name(index)
+        else:
+            return self._regions[:]
+
+    def region_by_name (self, name):
+        for region in self.regions():
+            if region.name == name:
+                return region
+
+        return None
+
+    def colours (self):
+        return self.colours
+
+    def glyphs (self):
+        return self.glyphs
+
+    def physical (self):
+        return self.screen
+
+    def blit (self):
+        """
+        This blits the entire contents of self.glyphs onto the screen -- using
+        colours where appropriate.
+        """
+
+        for coord, glyph in self.glyphs():
+            colour = self.colours().at(coord)
+
+            self.physical().put(coord, glyph, colour)
