60e95c3e2a14151e1c8e610138e9ea78d65300d9
Jon <jonathan@acss.net.au>
2011-03-05 12:04:04 +1000

    Make autogen wrapper-monkeypatch work properly.
    It's "rather" hacky, but it works.

--- docs/scripts/autogen.py
+++ docs/scripts/autogen.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
-
 import sys
 sys.path.append("../")
+from jinja2 import FileSystemLoader, Environment
+import sphinx.ext.autosummary.generate
 
-from sphinx.ext.autosummary.generate import *
-env = Environment(loader=PackageLoader('sphinx.ext.autosummary', '.templates'))
+def main ():
+    sphinx.ext.autosummary.generate.env = Environment(loader=FileSystemLoader('.templates'))
+    sphinx.ext.autosummary.generate.main(argv=sys.argv)
 
-main(argv=sys.argv)
+if __name__=="__main__":
+    main ()
